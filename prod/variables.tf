b7ef5323717e60c875a8701244425aa7669757b9
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-28 07:23:06 +0200

    variables

---
+++ prod/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  type    = string
+  description = "AWS region to create our resources"
+}
+
+variable "tag_env" {
+  description = "tag environment for out all resources"
+}
